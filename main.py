5ab68882ebc989d80f2a00cfecf4ebfae3ced430
mikasa <1244456115@qq.com>
2025-03-24 17:33:11 +0800

    init

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 
 import jmcomic
 
@@ -6,14 +7,15 @@ from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 
 
-@register("astrbot_plugin_jmcomic_downloader", "Sh1roCu", "JM下载", "v1.0.0", "https://github.com/Sh1roCu/astrbot_plugin_jmcomic_downloader")
+@register("astrbot_plugin_jmcomic_downloader", "Sh1roCu", "JM下载", "v1.0.0",
+          "https://github.com/Sh1roCu/astrbot_plugin_jmcomic_downloader")
 class JMComicDownloader(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     @filter.command("jm")
     async def execute_download(self, event: AstrMessageEvent, id: int):
-        option: jmcomic.JmOption = jmcomic.create_option_by_file("option.yml")
+        option: jmcomic.JmOption = jmcomic.create_option_by_file(sys.path[0] + "/option.yml")
         result: tuple = jmcomic.download_album(id, option)
         downloader: jmcomic.JmDownloader = result[1]
         while True:
